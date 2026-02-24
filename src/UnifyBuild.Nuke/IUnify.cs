1db8a118a9e4f7afcf0bae6f458044563c94bbed
ApprenticeGC <apprentice@giantcroissant.com>
2026-02-24 08:38:27 +0800

    refactor: remove old src structure and move to dotnet folder
    - Remove src/UnifyBuild.Nuke and src/UnifyBuild.Tool projects
    - Remove root UnifyBuild.sln
    - Remove .hub-manifest.toml
    - Prepare for new dotnet-based project structure

--- src/UnifyBuild.Nuke/IUnify.cs
+++
@@ -1,9 +0,0 @@
-namespace UnifyBuild.Nuke;
-
-/// <summary>
-/// Convenience interface that composes all UnifyBuild components.
-/// Equivalent to the old UnifyBuildBase but as an interface.
-/// </summary>
-public interface IUnify : IUnifyPublish, IUnifyPack
-{
-}
