6107d0754bb512d973c9c4d803a7fb2801773283
ApprenticeGC <apprentice@giantcroissant.com>
2026-03-02 19:07:01 +0800

    feat: add Fastlane integration for iOS/Android mobile builds

---
+++ mobile/android/sample-app/src/main/java/com/example/unifysample/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.unifysample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
